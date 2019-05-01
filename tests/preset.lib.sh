fa4c3a82bd5910d0fee81d4b360ca1929051ba88
Philippe Troin <phil+github-commits@fifi.org>
2019-05-01 09:34:26 -0700

    Update preset regressions for Solaris, FreeBSD.
    * tests/preset-alltimes.out.gold:
    * tests/preset-amtimes.out.gold:
    * tests/preset-contents.out.gold:
    * tests/preset-hardlinks.out.gold:
    * tests/preset-mode.out.gold:
    * tests/preset-mtime.out.gold:
    * tests/preset-notimes.out.gold:
    * tests/preset-owner.out.gold:
    Update for FreeBSD, Solaris and the changes to filtering.
    
    * tests/preset-alltimes.test:
    * tests/preset-amtimes.test:
    * tests/preset-contents.test:
    * tests/preset-mtime.test:
    * tests/preset-notimes.test:
    Filter by entry1_filter and optionally toplevel_atime_filter.
    
    * tests/preset.lib.sh (entry1_filter): Create: Filter out the
    directory vs. file blocks and size comparison.  The exact block
    count and size may be the same (and not part of the diff) or be
    different on different file systems.
    (toplevel_atime_filter): Create: Filter out the atime on the
    top-level directory.  They will be the same on Linux with the
    various default nodiratime mount options by default, but will be
    different on traditional Unix.
    (setup): Switch permissions around on entry1, now 700 for dir and
    600 for file.  Sync after writes for Solaris Bug.

--- tests/preset.lib.sh
+++ tests/preset.lib.sh
@@ -4,7 +4,7 @@ set -eu
 
 setup() {
   echo "File 1" > "$1"/entry1
-  chmod 700 "$1"/entry1
+  chmod 600 "$1"/entry1
   echo foo > "$1"/entry2
   ln "$1"/entry2 "$1"/entry3
   echo bar > "$1"/entry4
@@ -12,11 +12,24 @@ setup() {
   touch "$1"/dir1/missing
 
   mkdir "$2"/entry1
-  chmod 600 "$2"/entry1
+  chmod 700 "$2"/entry1
   echo foo > "$2"/entry2
   echo foo > "$2"/entry3
   dd bs=64k count=4 if=/dev/zero of="$2"/entry4
   mkdir "$2"/dir1
   touch "$2"/missing
 
+  # On Solaris block counts aren't updated until they're on storage.
+  sync
+}
+
+preset_filter() {
+ #$ Combining both sed statements fails on FreeBSD 12.0p3
+  sed -e '/^tdiff: entry1: blocks: /d' \
+      -e '/^tdiff: entry1: size: /d' \
+      -e 's!^\(tdiff: entry4: blocks:\) [0-9][0-9]* [0-9][0-9]*!\1 XX XX!'
+}
+
+toplevel_atime_filter() {
+  sed -e '/^tdiff: (top-level): atime: /d'
 }
